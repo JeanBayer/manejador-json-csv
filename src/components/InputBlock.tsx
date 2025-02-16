1fae1767dbec772d2c0b748b88c0c700bb902fad
jmoraba_bci <jhan.mora@bci.cl>
2025-02-15 22:52:56 -0300

    refactor InputBlock and OutputBlock components to include labels for better accessibility and organization

--- src/components/InputBlock.tsx
+++ src/components/InputBlock.tsx
@@ -16,17 +16,23 @@ export const InputBlock: React.FC<InputBlockProps> = ({
 }) => {
   return (
     <div className="flex flex-col flex-1 gap-4">
-      <Input
-        value={valueMatch}
-        onChange={(e) => setValueMatch(e.target.value)}
-        className="max-w-64"
-      />
-      <Textarea
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Pega aquí tus datos"
-        className="h-40"
-      />
+      <label className="block text-sm font-medium text-gray-700">
+        Match Field
+        <Input
+          value={valueMatch}
+          onChange={(e) => setValueMatch(e.target.value)}
+          className="mt-1 block w-full"
+        />
+      </label>
+      <label className="block text-sm font-medium text-gray-700">
+        Data Input
+        <Textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Pega aquí tus datos"
+          className="mt-1 block w-full h-40"
+        />
+      </label>
     </div>
   );
 };
--- src/components/OutputBlock.tsx
+++ src/components/OutputBlock.tsx
@@ -1,6 +1,6 @@
-import { SelectInput } from "@/components/wrapper/selectInput";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button"; // Asegúrate de importar tu componente Button
+import { Textarea } from "@/components/ui/textarea";
+import { SelectInput } from "@/components/wrapper/selectInput";
 import { useCopyToClipboard } from "@/hooks/useCopyToClipboard";
 import { useDownloadFile } from "@/hooks/useDownloadFile";
 
@@ -20,37 +20,43 @@ export const OutputBlock: React.FC<OutputBlockProps> = ({
 
   return (
     <div className="flex flex-col gap-4">
-      <SelectInput
-        onValueChange={setOutputFormat}
-        defaultValue={outputFormat}
-        items={[
-          { value: "json", label: "JSON" },
-          { value: "csv", label: "CSV" },
-        ]}
-        className="max-w-64"
-      />
-      <div className="relative">
-        <Textarea
-          value={output || ""}
-          placeholder="output"
-          className="h-40"
-          readOnly
+      <label className="text-sm font-medium text-gray-700">
+        Output Format
+        <SelectInput
+          onValueChange={setOutputFormat}
+          defaultValue={outputFormat}
+          items={[
+            { value: "json", label: "JSON" },
+            { value: "csv", label: "CSV" },
+          ]}
+          className="max-w-64"
         />
-        <div className="absolute top-2 right-2 flex gap-2">
-          <Button
-            onClick={() => copyToClipboard(output)}
-            className="bg-blue-500 text-white"
-          >
-            Copy
-          </Button>
-          <Button
-            onClick={() => downloadFile(output, "output.txt")}
-            className="bg-green-500 text-white"
-          >
-            Download
-          </Button>
+      </label>
+      <label className="block text-sm font-medium text-gray-700">
+        Output Data
+        <div className="relative">
+          <Textarea
+            value={output || ""}
+            placeholder="output"
+            className="mt-1 block w-full h-40"
+            readOnly
+          />
+          <div className="absolute top-2 right-2 flex gap-2">
+            <Button
+              onClick={() => copyToClipboard(output)}
+              className="bg-blue-500 text-white"
+            >
+              Copy
+            </Button>
+            <Button
+              onClick={() => downloadFile(output, "output.txt")}
+              className="bg-green-500 text-white"
+            >
+              Download
+            </Button>
+          </div>
         </div>
-      </div>
+      </label>
     </div>
   );
 };
