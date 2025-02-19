f3f144339e36091eb4035df16103f68bde0738c3
jmoraba_bci <jhan.mora@bci.cl>
2025-02-19 10:55:48 -0300

    refactor useStateDebounce hook to use useState instead of useSessionStorage for improved flexibility

--- src/hooks/use-state-debounce.ts
+++ src/hooks/use-state-debounce.ts
@@ -1,11 +1,13 @@
-import { useDebounce, useSessionStorage } from "@uidotdev/usehooks";
+import { useDebounce } from "@uidotdev/usehooks";
+import { useState } from "react";
 
 export const useStateDebounce = (
   key: string,
   initialValue = "",
   duration = 500
 ) => {
-  const [field, setField] = useSessionStorage(key, initialValue);
+  // const [field, setField] = useSessionStorage(key, initialValue);
+  const [field, setField] = useState(initialValue);
   const debounceField = useDebounce(field, duration);
 
   return [field, setField, debounceField] as const;
